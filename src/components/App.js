3a48c768da2be75f88e5edf61e4e8ecdf4a77f76
Шацкий Станислав Александрович <shackiy.s@yandex.ru>
2019-04-17 14:49:35 +0300

    css

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import articles from '../fixtures';
-import ArticleList from './ArticleList';
+import React from 'react'
+import articles from '../fixtures'
+import ArticleList from './ArticleList'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 function App(){
 return (
-    <div>
-    <h1>App name</h1>
+    <div className="container">
+    <div className="jumbotron">
+    <h1 className="display-3">App name</h1>
+    </div>
     <ArticleList articles={articles}/>
 
     </div>
