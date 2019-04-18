7750d280201cb70953bc0490bb682886ea61ca17
DevLab <roma.ivanov.official@mail.ru>
2019-04-18 17:35:05 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import articles from '../fixtures'
 import ArticleList from './ArticleList'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
-class App extends Component(){
-   // state={
-        //reverted:false
-   // }
-    render(){
+class App extends Component {
+    state={
+        everted:false
+    }
+    render() {
         return (
-          <div className="container">
+            <div className="container">
                 <div className="jumbotron">
                     <h1>App name
                        <button className="btn" onClick={this.revert}>Revert</button>
                     </h1>
                 </div>
-                        <ArticleList articles={this.state.reverted ? articles.reverse(): articles}/>
+                <ArticleList articles={this.state.reverted ? articles.reverse() : articles} />
 
             </div>
-)
+        )
     }
-revert=()=>this.setState({
-    reverted:!this.state.reverted
-})
+    revert = () => this.setState({
+        reverted: !this.state.reverted
+    })
 }
 export default App
